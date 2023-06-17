eba6db32475478abf30863d7405bf26836af4928
Or1GG1n <json57283@gmail.com>
2023-06-17 15:39:23 +0300

    START

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    colors:{
+      'green-lz': '#2BAD72',
+      'red-lz': '#884444',
+      'white-lz': '#FFFFFF',
+      'black-lz': '#080808',
+      'signal-lz': '#2D2D2D',
+      'gray-brown-lz': '#363636',
+      'asphalt-lz': '#505050',
+      'gray-lz': '##272727',
+      'light-gray': '#303030',
+      'background-dark-mc': '#0d0d11',
+    },
+
+  },
+}
+
